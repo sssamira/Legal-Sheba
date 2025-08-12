61b042cc2385b0e3f892fed7f75e74c65be6c816
sssamira <samira.akter1@g.bracu.ac.bd>
2025-08-12 11:56:51 +0600

    initial commit

---
+++ legalsheba-backend/src/main/java/io/github/sssamira/legalsheba/LegalshebaBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.sssamira.legalsheba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalshebaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalshebaBackendApplication.class, args);
+	}
+
+}
---
+++ legalsheba-backend/src/test/java/io/github/sssamira/legalsheba/LegalshebaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sssamira.legalsheba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalshebaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
